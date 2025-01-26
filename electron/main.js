c09106856c0ed9e5ac70b763b54bf4cf22634163
yiseoffline <lsy0476@hufs.ac.kr>
2025-01-27 02:39:27 +0900

    fix: electron empty screen error

--- electron/main.js
+++ electron/main.js
@@ -1,6 +1,19 @@
 const { app, BrowserWindow } = require("electron/main");
 const path = require("node:path");
 
+// 개발 환경에서만 electron-reload 활성화
+if (process.env.ELECTRON_START_URL) {
+  require("electron-reload")(path.join(__dirname, ".."), {
+    electron: path.join(
+      __dirname,
+      "..",
+      "node_modules",
+      ".bin",
+      "electron" // electron 실행 파일 경로
+    ),
+  });
+}
+
 function createWindow() {
   const win = new BrowserWindow({
     width: 800,
@@ -10,12 +23,21 @@ function createWindow() {
     },
   });
 
-  // React 앱의 빌드 결과물을 로드
-  const startUrl =
-    process.env.ELECTRON_START_URL ||
-    `file://${path.join(__dirname, "../build/index.html")}`;
+  win.webContents.on("did-finish-load", () => {
+    if (process.env.ELECTRON_START_URL) {
+      win.webContents.reloadIgnoringCache(); // Electron 창 강제 새로 고침
+    }
+  });
 
+  // React 앱의 빌드 결과물을 로드
+  const startUrl = process.env.ELECTRON_START_URL || "http://localhost:3000";
   win.loadURL(startUrl);
+  win.webContents.openDevTools();
+  // const startUrl =
+  //   process.env.ELECTRON_START_URL ||
+  //   `file://${path.join(__dirname, "../build/index.html")}`;
+
+  // win.loadURL(startUrl);
 }
 
 app.whenReady().then(() => {
